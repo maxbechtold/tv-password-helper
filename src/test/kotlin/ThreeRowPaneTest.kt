dc62f39e073cf1704d17cad583c9e26cec2a76a8
Max Bechtold <maxbechtold@web.de>
2020-12-23 16:10:14 +0100

    chore: align line endings

--- src/test/kotlin/ThreeRowPaneTest.kt
+++ src/test/kotlin/ThreeRowPaneTest.kt
@@ -1,9 +1,6 @@
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.function.Executable
-import java.lang.IllegalArgumentException
 
 internal class ThreeRowPaneTest {
 
